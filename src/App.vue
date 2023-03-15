5cb09184f79c923323e440aa7d7a79537a95a895
George Ukolov <ukolovgeorge1999@gmail.com>
2023-03-15 21:20:02 +0300

    edited App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,4 @@
+
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
@@ -7,9 +8,16 @@
 
 <script>
 
+import ProductList from './components/ProductList.vue'
+import Cart from './components/Cart.vue'
+
 export default {
   name: 'App',
   components: {
+    // eslint-disable-next-line vue/no-unused-components
+    ProductList,
+    // eslint-disable-next-line vue/no-unused-components
+    Cart
   }
 }
 </script>
